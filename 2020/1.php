97d57154651b959d5a3088277962b8148d8867a2
Einacio <pablo@loop-sistemas.com.ar>
2020-12-08 15:29:10 +0100

    2020 day 1, both answers

--- 2020/1.php
+++ 2020/1.php
@@ -1,9 +1,9 @@
 <?php
-if ($argc == 1){
+if ($argc == 1) {
     echo '
-    Usage: 1.php 1|2 datafile
-     First parameter is the part of the puzzle, the second is the file with the data.
-     Echoes the correct final value
+    Usage: 1.php datafile
+     The parameter is the file with the data.
+     Echoes the correct final value for both puzzles
     ';
     exit;
 }
@@ -13,39 +13,38 @@ $values = array_flip($values); // convert values into indices, that give us easi
 
 // polyfill pre 7.3
 if (!function_exists('array_key_first')) {
-    function array_key_first(array $arr) {
-        foreach($arr as $key => $unused) {
+    function array_key_first(array $arr)
+    {
+        foreach ($arr as $key => $unused) {
             return $key;
         }
-        return NULL;
+
+        return null;
     }
 }
 
 
-if($argv[1] == 1){
-    foreach ($values as $n => $i){
-        // check if the complement exists
-        $counterPart = 2020-$n;
-        if(isset($values[$counterPart])){
-            echo $n * $counterPart;
+foreach ($values as $n => $i) {
+    // check if the complement exists
+    $counterPart = 2020 - $n;
+    if (isset($values[$counterPart])) {
+        echo $n * $counterPart . "\n";
+        break;
+    }
+}
+
+while (count($values) > 3) {
+    // take out the first value in the list
+    $currentPivot = array_key_first($values);
+    unset($values[$currentPivot]);
+
+    //same logic but with the extra first value
+    foreach ($values as $n => $i) {
+        // find complement to the 2 values we know
+        $counterPart = 2020 - ($n + $currentPivot);
+        if (isset($values[$counterPart])) {
+            echo $n * $counterPart * $currentPivot;
             exit;
         }
     }
-}else{
-    while(count($values) > 3) {
-        // take out the first value in the list
-        $currentPivot = array_key_first($values);
-        unset($values[$currentPivot]);
-
-        //same logic but with the extra first value
-        foreach ($values as $n => $i){
-            // find complement to the 2 values we know
-            $counterPart = 2020 - ($n + $currentPivot);
-            if(isset($values[$counterPart])){
-                echo $n * $counterPart * $currentPivot;
-                exit;
-            }
-        }
-    }
-
 }
