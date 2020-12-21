596a40d904c6b9c2bc67171610d010594a853bd4
Einacio <pablo@loop-sistemas.com.ar>
2020-12-21 20:32:17 +0100

    2020 day 21 part 2

--- 2020/21.php
+++ 2020/21.php
@@ -6,12 +6,12 @@ $ingredients = [];
 $allergens = [];
 
 foreach ($values as $line){
-    list($ing, $adv) = explode(' (contains ', $line);
+    [$ing, $adv] = explode(' (contains ', $line);
     $ing = explode(' ', $ing);
     $adv = explode(', ', substr($adv, 0, -1));
     foreach ($ing as $i){
         if(!isset($ingredients[$i])){
-            $ingredients[$i] = ['count'=>1, 'all'=>[]];
+            $ingredients[$i] = ['count'=>1, 'all'=>[], 'canbe'=>[]];
         }else{
             $ingredients[$i]['count']++;
         }
@@ -34,13 +34,36 @@ foreach ($values as $line){
 
 
 $clean = [];
-foreach ($ingredients as $name => $ing){
+foreach ($ingredients as $name => &$ing){
     foreach ($ing['all'] as $al => $count){
-        if($allergens[$al] <= $count){
-            continue 2;
+        if($allergens[$al] == $count){
+            $ing['canbe'][] = $al;
         }
     }
-    $clean[$name] = $ing['count'];
+    if(!$ing['canbe']){
+        $clean[$name] = $ing['count'];
+        unset($ingredients[$name]);
+    }
 }
 
 echo '1: '.array_sum($clean)."\n";
+
+$is = [];
+while($allergens) {
+    foreach ($ingredients as $name => &$ing) {
+        if(!$ing['canbe']) continue;
+        foreach($ing['canbe'] as $i=>$prob){
+            if(!isset($allergens[$prob])){
+                unset($ing['canbe'][$i]);
+            }
+        }
+        if(count($ing['canbe']) == 1){
+            $al = reset($ing['canbe']);
+            $is[$al] = $name;
+            unset($allergens[$al]);
+        }
+    }
+}
+
+ksort($is);
+echo '2: '.implode(',', $is)."\n";
