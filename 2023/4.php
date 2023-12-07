202c98488aa1a129d794219928bab96a7a8aaf6c
Einacio <pablo@loop-sistemas.com.ar>
2023-12-07 22:20:05 +0100

    2023 day 4 part 2

--- 2023/4.php
+++ 2023/4.php
@@ -6,11 +6,16 @@ prelude();
 
 $values = in();
 
-//part 1
+//part 1 & 2
 
-$total = 0;
+$total1 = 0;
+
+$copies = [];
+$currentCard = 0;
+$maxCard = count($values);
 
 foreach ($values as $row){
+    $currentCard++;
     [$_, $numbers] = explode(': ', $row);
     [$winning, $have] = explode(' | ', $numbers);
     $winning = explode(',', preg_replace('/\s+/', ',', trim($winning)));
@@ -21,8 +26,18 @@ foreach ($values as $row){
     debug(json_encode($matches));
     $mc = count($matches);
     if($mc) {
-        $total += pow(2, $mc-1);
+        $total1 += pow(2, $mc-1);
+    }
+    $copies[$currentCard] = ($copies[$currentCard]??0) + 1;
+
+    while($mc){
+        if($currentCard+$mc <= $maxCard){
+            $copies[$currentCard+$mc] = ($copies[$currentCard+$mc]??0) + $copies[$currentCard];
+        }
+        $mc--;
     }
 }
+$total2 = array_sum($copies);
 
-out("1: {$total}");
+out("1: {$total1}");
+out("2: {$total2}");
